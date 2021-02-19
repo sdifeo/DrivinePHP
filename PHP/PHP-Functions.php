beb4ec2edb76718d17631ef4afc99f4870c5611b
Steven Di Feo (1934386) <>
2021-02-19 16:09:48 -0500

    Steven Di Feo Getting some more of the HTML/CSS done, as well as some functions. Feb 19, 2021

--- PHP/PHP-Functions.php
+++ PHP/PHP-Functions.php
@@ -1,15 +1,17 @@
 <?php
 
+define("FOLDER_CSS", "CSS/");
+define("FILE_CSS_STYLES_INDEX", FOLDER_CSS . "index_style.css");
 
-#used to load up the main body - HTML tags like DOCTYPE, body, title, etc
 function createPageHeader($title)
 {
     ?>
     <html>
         <head>
             <meta charset="UTF-8">
-            <title>$title</title>
-            <link rel="stylesheet" href="<?php echo FILE_CSS_STYLES; ?>">
+            <title><?php echo $title ?></title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="<?php echo FILE_CSS_STYLES_INDEX; ?>">
         </head>
         <body>
             
@@ -23,14 +25,26 @@ function createNavigationBar()
     <div class="navigationBar">
             <div class="nav-Buttons">
                 <ol>
-                    <li> <a href="#">Home</a></li>
-                    <li><a href="#">Home</a></li>
+                    <li id="NB-B1"> <a href="#"><span>Home</span></a></li>
+                    <li id="NB-B2"><a href="#">Home</a></li>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Home</a></li>
                 </ol>
             </div>
+        
+        <hr id="navBar-HLine">
     </div>    
         <?php
+}
+
+
+function displayWebsiteLogo()
+{
+    ?>
+            <div>
+                <img href="">
+            </div>
+    <?php
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,22 +1,29 @@
 <?php
-
+//PHP functions
     require_once "PHP/PHP-Functions.php";
-
     define("FOLDER_PHP", "PHP/");
     define("FILE_PHP_FUNCTIONS", FOLDER_PHP . "PHP-Functions.php");
+    
+//IMAGES
+    define("FOLDER_IMAGES", "IMAGES/");
 ?>
 
 <!DOCTYPE html>
 
 
 <?php
-
 createPageHeader("Home Page");
-createNavigationBar();
-
 ?>
 
+<div class="imgBG">
+<?php
+createNavigationBar();
+?>
+</div>
+
 Hello there
+
+<img id="LC-500-indexHeader" src="./IMAGES/CARS/indexHeader.jpg" alt="N/A"/>
 
     </body>
 </html>
