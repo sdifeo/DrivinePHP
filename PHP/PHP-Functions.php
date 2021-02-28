7f641161058e24694e961cff9fb3e73399bd85d4
Steven Di Feo (1934386) <15142@192.168.2.86>
2021-02-28 00:07:06 -0500

    Steven Di Feo Adding logo and adding small details to index. Will be adding ADs soon. Comments added too. Feb 28, 2021

--- PHP/PHP-Functions.php
+++ PHP/PHP-Functions.php
@@ -3,6 +3,9 @@
 define("FOLDER_CSS", "CSS/");
 define("FILE_CSS_STYLES_INDEX", FOLDER_CSS . "index_style.css");
 
+define("FOLDER_IMAGES", "IMAGES/");
+define("FILE_LOGO", FOLDER_IMAGES . "/LOGO/DRIVINE.png");
+
 function createPageHeader($title)
 {
     ?>
@@ -11,14 +14,14 @@ function createPageHeader($title)
             <meta charset="UTF-8">
             <title><?php echo $title ?></title>
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="<?php echo FILE_CSS_STYLES_INDEX; ?>">
+            <link rel="stylesheet" href="<?php echo FILE_CSS_STYLES_INDEX ?>">
             <link rel="preconnect" href="https://fonts.gstatic.com">
             <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
         </head>
         <body>
             
         
-        <?php
+    <?php
 }
 
 function createNavigationBar()
@@ -27,6 +30,7 @@ function createNavigationBar()
     <div class="navigationBar">
             <div class="nav-Buttons">
                 <ol>
+                    <li id="li-Logo-Drivine"><img id="logo-Drivine" src="<?php echo "FILE_LOGO"?>"></li>
                     <li id="NB-B1"> <a href="#"><span>Home</span></a></li>
                     <li id="NB-B2"><a href="#">Home</a></li>
                     <li><a href="#">Home</a></li>
@@ -49,7 +53,4 @@ function displayWebsiteLogo()
     <?php
 }
 
-function 
-
-?>
 
--- index.php
+++ index.php
@@ -1,6 +1,17 @@
+<!-- TO DO: HOMEPAGE
+DISPLAY LOGO
+GIVE BRIEF DESCRIPTION OF WEBSITE
+DISPLAY MAIN HEADER IMAGE (LEXUS?) - GIVE BRIEF DESCRIPTION OF CAR
+
+CONTENT:
+SHOW CARS/BIKES WITH BRIEF DESCRIPTION
+CREATE ADVERTISMENT SECTION - BOTTOM OF PAGE?
+-->
+
 <?php
 //PHP functions
     require_once "PHP/PHP-Functions.php";
+    
     define("FOLDER_PHP", "PHP/");
     define("FILE_PHP_FUNCTIONS", FOLDER_PHP . "PHP-Functions.php");
     
@@ -15,15 +26,23 @@
 createPageHeader("Home Page");
 ?>
 
+
+
 <div class="imgBG">
 <?php
 createNavigationBar();
 ?>
+    
+    <p id="headerIntro">Welcome to <span id="drivineName">Drivine</span>. Delivering superb quality 
+        and prices, here, you will find the vehicle of your dreams. Simply Divine.
+    </p>
 </div>
 
-Hello there
-
 <img id="LC-500-indexHeader" src="./IMAGES/CARS/indexHeader.jpg" alt="N/A"/>
+
+<div>
+    
+</div>
 
     </body>
 </html>
